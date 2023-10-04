b43746f9326d97c9360cbd122fa9fdf6d9bd009b
Salo Deatcu <46715729+salo-dea@users.noreply.github.com>
2023-10-04 21:06:23 +0200

    Add cursor movement, return to enter and sorting files separately from directories

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,5 @@
 //TODO:
-// print full cwd
-// allow going up
-// implement cursor
-//  - text search for fast nav?
+// text search for fast nav?
 
 const std = @import("std");
 const ncurses = @cImport({
@@ -13,52 +10,75 @@ const NcursesError = error{
     Generic,
 };
 
-const WormholeErrors = error{
-    NoParent,
+const WormholeErrors = error{ NoParent, NoDir };
+
+const File = struct {
+    path: []u8,
+    kind: std.fs.File.Kind,
 };
 
 const DirExplorer = struct {
+    const Self = @This();
     current_dir: []u8,
     allocator: std.mem.Allocator,
-    contents: std.ArrayList([]const u8),
+    contents: std.ArrayList(File),
+    cursor: i32 = 0,
 
     pub fn init(allocator: std.mem.Allocator, start_dir: []const u8) !DirExplorer {
         return DirExplorer{ .current_dir = try std.fs.cwd().realpathAlloc(allocator, "."), .contents = try listdir(allocator, start_dir), .allocator = allocator };
     }
-
-    fn update_contents(self: *DirExplorer) !void {
+    pub fn deinit(self: *Self) void {
+        self.allocator.free(self.current_dir);
         self.contents.deinit();
-        self.contents = try listdir(self.allocator, self.current_dir);
     }
 
-    pub fn go_up(self: *DirExplorer) !void {
+    fn refresh(self: *Self) !void {
+        self.contents.deinit();
+        self.contents = try listdir(self.allocator, self.current_dir);
+        self.cursor = 0;
+    }
+
+    pub fn move_cursor(self: *Self, move: i32) void {
+        const max_cursor: i32 = @intCast(self.contents.items.len);
+        self.cursor = @mod(self.cursor + move, max_cursor);
+    }
+
+    pub fn get_cursor(self: Self) usize {
+        return @intCast(self.cursor);
+    }
+
+    pub fn go_up(self: *Self) !void {
         var i = self.current_dir.len - 1;
         while (i > 0) : (i -= 1) {
             if (self.current_dir[i] == '/') {
                 self.current_dir.len = i;
-                try self.update_contents();
+                try self.refresh();
                 return;
             }
         }
         return WormholeErrors.NoParent;
     }
 
-    pub fn enter(self: *DirExplorer, target_dir: []const u8) !void {
-        const last_dir = self.current_dir;
-        defer self.allocator.free(last_dir);
+    pub fn enter(self: *Self, target_dir: File) !void {
+        if (target_dir.kind == std.fs.File.Kind.directory) {
+            const last_dir = self.current_dir;
+            defer self.allocator.free(last_dir);
 
-        self.current_dir = try std.fmt.allocPrint(self.allocator, "{s}/{s}", .{ self.current_dir, target_dir });
-        try self.update_contents();
+            self.current_dir = try std.fmt.allocPrint(self.allocator, "{s}/{s}", .{ self.current_dir, target_dir.path });
+            try self.refresh();
+        } else {
+            return WormholeErrors.NoDir;
+        }
     }
 };
 
 pub fn main() !void {
     //try mytest();
-    var alloc = std.heap.c_allocator; // std.heap.GeneralPurposeAllocator(.{}).allocator();
+    var alloc = std.heap.page_allocator; // std.heap.GeneralPurposeAllocator(.{}).allocator();
     try print_dir_contents(alloc);
 }
 
-fn getch() NcursesError!u8 {
+fn getch() NcursesError!usize {
     var val = ncurses.getch();
     if (val < 0) {
         return NcursesError.Generic;
@@ -77,40 +97,54 @@ fn print_dir_contents(alloc: std.mem.Allocator) !void {
     var win = ncurses.initscr();
     _ = win;
     defer _ = ncurses.endwin();
+
+    _ = ncurses.keypad(ncurses.stdscr, true);
     _ = ncurses.noecho();
     _ = ncurses.cbreak();
 
     var dir_exp = try DirExplorer.init(alloc, ".");
-    while (true) {
+    defer dir_exp.deinit();
+
+    main_loop: while (true) {
         try ncurse_print(alloc, "-> {s} \n", .{dir_exp.current_dir});
 
         const dirs = &dir_exp.contents;
         for (0.., dirs.items) |i, dir| {
             //defer alloc.free(cstring);
-            try ncurse_print(alloc, "[{d}] {s} \n", .{ i, dir });
+            const highlighted: bool = i == dir_exp.cursor;
+
+            if (highlighted) {
+                _ = ncurses.attron(ncurses.COLOR_PAIR(5) | ncurses.A_BOLD);
+            }
+
+            switch (dir.kind) {
+                .directory => try ncurse_print(alloc, "[{d}] {s} \n", .{ i, dir.path }),
+                else => try ncurse_print(alloc, " - {s} \n", .{dir.path}),
+            }
+
+            if (highlighted) {
+                _ = ncurses.attroff(ncurses.COLOR_PAIR(5) | ncurses.A_BOLD);
+            }
         }
         _ = ncurses.refresh();
         var key: usize = getch() catch 255;
         _ = ncurses.move(0, 0); //reset cursor
         _ = ncurses.clear();
-        if (key < '0') {
-            std.debug.print("BREAK_ERLY \n", .{});
-            break;
+
+        switch (key) {
+            '0'...'9' => {
+                const idx = key - '0';
+                try dir_exp.enter(dirs.items[idx]);
+            },
+            ncurses.KEY_DOWN => dir_exp.move_cursor(1),
+            ncurses.KEY_UP => dir_exp.move_cursor(-1),
+            '\n' => try dir_exp.enter(dirs.items[dir_exp.get_cursor()]),
+            ncurses.KEY_LEFT, ncurses.KEY_BACKSPACE => try dir_exp.go_up(),
+            std.ascii.control_code.esc => break :main_loop,
+            else => std.debug.print("UNKNOWN KEY: {d} \n", .{key}),
         }
 
-        if (key == 'u') {
-            try dir_exp.go_up();
-            continue;
-        }
-
-        key -= '0';
-
-        if (key < dirs.items.len) {
-            try dir_exp.enter(dirs.items[key]);
-        } else {
-            std.debug.print("BREAK\n", .{});
-            break;
-        }
+        //_ = ncurses.mvchgat(selection, 0, -1, ncurses.A_UNDERLINE, @intCast(ncurses.COLOR_PAIR(ncurses.COLOR_WHITE)), null);
     }
 }
 
@@ -125,48 +159,24 @@ fn str_less_than(context: void, str_a: []const u8, str_b: []const u8) bool {
     return str_a.len < str_b.len;
 }
 
-fn listdir(alloc: std.mem.Allocator, dirname: []const u8) !std.ArrayList([]const u8) {
+fn file_less_than(context: void, file_a: File, file_b: File) bool {
+
+    //directories first
+    if (file_a.kind != file_b.kind and (file_a.kind == std.fs.File.Kind.directory or file_b.kind == std.fs.File.Kind.directory)) {
+        return file_a.kind == std.fs.File.Kind.directory;
+    }
+    // everything else sorted by name
+    return str_less_than(context, file_a.path, file_b.path);
+}
+
+fn listdir(alloc: std.mem.Allocator, dirname: []const u8) !std.ArrayList(File) {
     const dir = try std.fs.cwd().openIterableDir(dirname, .{});
     var iterator = dir.iterate();
-
-    var dirlist: std.ArrayList([]const u8) = std.ArrayList([]const u8).init(alloc);
+    var dirlist: std.ArrayList(File) = std.ArrayList(File).init(alloc);
 
     while (try iterator.next()) |path| {
-        //        try dirlist.append(path.name); //try std.fmt.allocPrint(alloc, "{s}", .{path.name}));
-        try dirlist.append(try std.fmt.allocPrint(alloc, "{s}", .{path.name}));
+        try dirlist.append(File{ .path = try std.fmt.allocPrint(alloc, "{s}", .{path.name}), .kind = path.kind });
     }
-    std.sort.block([]const u8, dirlist.items, {}, str_less_than);
+    std.sort.block(File, dirlist.items, {}, file_less_than);
     return dirlist;
-}
-
-fn mytest() !void {
-    var alloc = std.heap.page_allocator;
-    var dirs = try listdir(alloc, ".");
-    _ = ncurses.initscr();
-    defer _ = ncurses.endwin();
-    //var dirs2 = try listdir(".");
-    //_ = dirs2;
-    var buf = try std.fmt.allocPrint(alloc, "{s}", .{dirs.items[0]});
-    var bufZ = try std.fmt.allocPrintZ(alloc, "[{d}] {s}", .{ 1, buf });
-    std.debug.print("buf : {s}\n", .{buf});
-    std.debug.print("bufZ : {s}\n", .{bufZ});
-    _ = ncurses.initscr();
-    defer _ = ncurses.endwin();
-    std.debug.print("buf : {s}\n", .{buf});
-    std.debug.print("bufZ : {s}\n", .{bufZ});
-}
-
-test "mytest" {
-    _ = ncurses.initscr();
-    var dirs = try listdir(".");
-    _ = ncurses.endwin();
-    var alloc = std.heap.page_allocator;
-    var buf = try std.fmt.allocPrint(alloc, "{s}", .{dirs.items[0]});
-    var bufZ = try std.fmt.allocPrintZ(alloc, "[{d}] {s}", .{ 1, buf });
-    std.debug.print("buf : {s}", .{buf});
-    std.debug.print("bufZ : {s}", .{bufZ});
-    _ = ncurses.initscr();
-    defer _ = ncurses.endwin();
-    std.debug.print("buf : {s}", .{buf});
-    std.debug.print("bufZ : {s}", .{bufZ});
 }
