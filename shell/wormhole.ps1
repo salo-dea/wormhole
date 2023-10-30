2853be7f4acc2c1040a0c9af4425e560b7af0600
salo-dea <46715729+salo-dea@users.noreply.github.com>
2023-10-30 18:33:59 +0100

    provide powershell script that enables direct navigation to the target location (can be copied into a function)

---
+++ shell/wormhole.ps1
@@ -0,0 +1,27 @@
+
+while($true) {
+    $nav_file = ".fastnav-wormhole"
+    $dirty_nav_state =  Test-Path $nav_file -PathType Leaf
+    if($dirty_nav_state)
+    {
+        Write-Warning "$nav_file exists already! Cleaning up!"
+        Remove-Item $nav_file #clean up if there was something left before
+    }
+    
+    ./wormhole.exe
+    $target_path = Get-Content .\.fastnav-wormhole
+    $is_folder = Test-Path $target_path -PathType Container
+    $is_file = Test-Path $target_path -PathType Leaf
+    
+    Remove-Item $nav_file
+    if ($is_folder){
+        Set-Location $target_path
+        break
+    }
+    elseif ($is_file){
+        Invoke-Item $target_path # open with default app
+    }
+    else {
+        Write-Error "Invalid Path"
+    }
+}
